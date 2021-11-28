443f87eb3080d6a46c7322ec54bc2180f38e2f47
elias <eliasenglen2@gmail.com>
2021-11-28 12:31:15 +0100

    add new

--- src/components/Weather.vue
+++ src/components/Weather.vue
@@ -23,9 +23,6 @@
 
 <script>
 import axios from 'axios'
-import am4charts from '@amcharts/amcharts4/charts'
-import am4core from '@amcharts/amcharts4/core'
-import am4themes_animated from '@amcharts/amcharts4/themes/animated'
 
 export default {
     name: "Weather",
@@ -36,74 +33,6 @@ export default {
         }
     },
     methods: {
-        createSparkLine() {
-            am4core.useTheme(am4themes_animated);
-
-            var chart = am4core.create("chartdiv", am4charts.XYChart);
-
-            chart.data = [ {
-                    "date": new Date(2018, 0, 1, 8, 0, 0),
-                    "value": 57
-                }, {
-                    "date": new Date(2018, 0, 1, 9, 0, 0),
-                    "value": 27
-                }, {
-                    "date": new Date(2018, 0, 1, 10, 0, 0),
-                    "value": 24
-                }, {
-                    "date": new Date(2018, 0, 1, 11, 0, 0),
-                    "value": 59
-                }, {
-                    "date": new Date(2018, 0, 1, 12, 0, 0),
-                    "value": 33
-                }, {
-                    "date": new Date(2018, 0, 1, 13, 0, 0),
-                    "value": 46
-                }, {
-                    "date": new Date(2018, 0, 1, 14, 0, 0),
-                    "value": 20
-                }, {
-                    "date": new Date(2018, 0, 1, 15, 0, 0),
-                    "value": 42
-                }, {
-                    "date": new Date(2018, 0, 1, 16, 0, 0),
-                    "value": 59,
-                "opacity": 1
-                }
-            ];
-
-            chart.padding(0, 0, 0, 0);
-
-            var dateAxis = chart.xAxes.push(new am4charts.DateAxis());
-            dateAxis.renderer.grid.template.disabled = true;
-            dateAxis.renderer.labels.template.disabled = true;
-            dateAxis.startLocation = 0.5;
-            dateAxis.endLocation = 0.7;
-            dateAxis.cursorTooltipEnabled = false;
-
-            var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
-            valueAxis.min = 0;
-            valueAxis.renderer.grid.template.disabled = true;
-            valueAxis.renderer.baseGrid.disabled = true;
-            valueAxis.renderer.labels.template.disabled = true;
-            valueAxis.cursorTooltipEnabled = false;
-
-            chart.cursor = new am4charts.XYCursor();
-            chart.cursor.lineY.disabled = true;
-
-            var series = chart.series.push(new am4charts.LineSeries());
-            series.tooltipText = "{date}: [bold]{value}";
-            series.dataFields.dateX = "date";
-            series.dataFields.valueY = "value";
-            series.tensionX = 0.8;
-            series.strokeWidth = 2;
-
-            // render data points as bullets
-            var bullet = series.bullets.push(new am4charts.CircleBullet());
-            bullet.circle.opacity = 0;
-            bullet.circle.propertyFields.opacity = "opacity";
-            bullet.circle.radius = 3;
-        },
         SpliceTime(str) {
             return str.substr(11).slice(0, -3)
         },
@@ -155,7 +84,7 @@ export default {
 }
 
 .temperature {
-    font-size: 40px;
+    font-size: 30px;
     font-weight: 600;
     padding: 0;
     margin: 0;
